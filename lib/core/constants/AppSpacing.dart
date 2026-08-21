ab39c8455b59254291d7a8c30b7d5f03346ed97d
moulaye_d <moulayedagnon@gmail.com>
2026-08-21 14:34:19 +0000

    model utilisateur et enfant

---
+++ lib/core/constants/AppSpacing.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class AppSpacing {
+
+  static const double xs = 4.0;
+  static const double sm = 8.0;
+  static const double md = 12.0;
+  static const double lg = 16.0;
+  static const double xl = 24.0;
+  static const double xxl = 32.0;
+
+
+  static const SizedBox verticalGapXs = SizedBox(height: xs);
+  static const SizedBox verticalGapSm = SizedBox(height: sm);
+  static const SizedBox verticalGapMd = SizedBox(height: md);
+  static const SizedBox verticalGapLg = SizedBox(height: lg);
+  static const SizedBox verticalGapXl = SizedBox(height: xl);
+  static const SizedBox verticalGapXxl = SizedBox(height: xxl);
+
+
+  static const SizedBox horizontalGapSm = SizedBox(width: xs);
+  static const SizedBox horizontalGapMd = SizedBox(width: md);
+  static const SizedBox horizontalGapLg = SizedBox(width: lg);
+  static const SizedBox horizontalGapXl = SizedBox(width: xl);
+  static const SizedBox horizontalGapXxl = SizedBox(width: xxl);
+}
+
