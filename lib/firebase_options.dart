6e0aa574d005249e71edd2962b7c69f8ea5d6c31
ZakariaMAIGA <158841471+ZakariaMAIGA@users.noreply.github.com>
2026-08-24 08:15:14 +0000

    Revert "ajout de feature cart"

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -17,17 +17,29 @@ import 'package:flutter/foundation.dart'
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     if (kIsWeb) {
-      return web;
+      throw UnsupportedError(
+        'DefaultFirebaseOptions have not been configured for web - '
+        'you can reconfigure this by running the FlutterFire CLI again.',
+      );
     }
     switch (defaultTargetPlatform) {
       case TargetPlatform.android:
         return android;
       case TargetPlatform.iOS:
-        return ios;
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for ios - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
       case TargetPlatform.macOS:
-        return macos;
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for macos - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
       case TargetPlatform.windows:
-        return windows;
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for windows - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
       case TargetPlatform.linux:
         throw UnsupportedError(
           'DefaultFirebaseOptions have not been configured for linux - '
@@ -46,42 +58,5 @@ class DefaultFirebaseOptions {
     messagingSenderId: '194931270842',
     projectId: 'eveiloo-flutter',
     storageBucket: 'eveiloo-flutter.firebasestorage.app',
-  );
-  static const FirebaseOptions web = FirebaseOptions(
-    apiKey: 'AIzaSyDEckM_HqfvhxPzKfTkPqF4tNh7KdflBzU',
-    appId: '1:194931270842:web:ec73cf84204d2bf1c98e83',
-    messagingSenderId: '194931270842',
-    projectId: 'eveiloo-flutter',
-    authDomain: 'eveiloo-flutter.firebaseapp.com',
-    storageBucket: 'eveiloo-flutter.firebasestorage.app',
-    measurementId: 'G-946LLZF0CX',
-  );
-
-  static const FirebaseOptions macos = FirebaseOptions(
-    apiKey: 'AIzaSyBvPp4rUudAtxyNIv_GPhIuHYoMZkeDdz8',
-    appId: '1:194931270842:ios:51af81a491db3dc4c98e83',
-    messagingSenderId: '194931270842',
-    projectId: 'eveiloo-flutter',
-    storageBucket: 'eveiloo-flutter.firebasestorage.app',
-    iosBundleId: 'com.example.eveilooEnfant',
-  );
-
-  static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: 'AIzaSyBvPp4rUudAtxyNIv_GPhIuHYoMZkeDdz8',
-    appId: '1:194931270842:ios:51af81a491db3dc4c98e83',
-    messagingSenderId: '194931270842',
-    projectId: 'eveiloo-flutter',
-    storageBucket: 'eveiloo-flutter.firebasestorage.app',
-    iosBundleId: 'com.example.eveilooEnfant',
-  );
-
-  static const FirebaseOptions windows = FirebaseOptions(
-    apiKey: 'AIzaSyDEckM_HqfvhxPzKfTkPqF4tNh7KdflBzU',
-    appId: '1:194931270842:web:011a7aaee859e77ac98e83',
-    messagingSenderId: '194931270842',
-    projectId: 'eveiloo-flutter',
-    authDomain: 'eveiloo-flutter.firebaseapp.com',
-    storageBucket: 'eveiloo-flutter.firebasestorage.app',
-    measurementId: 'G-1FTLKX02VZ',
   );
 }
--- lib/models/cart_model.dart
+++ lib/models/cart_model.dart
@@ -19,7 +19,7 @@ class CartItemModel {
   // Calcul du prix total pour cet article
   double get totalArticle => prixUnitaire * quantite;
 
-  factory CartItemModel.fromFirestore(Map<String, dynamic> json, String id, List<String> ids) {
+  factory CartItemModel.fromFirestore(Map<String, dynamic> json, String id) {
     return CartItemModel(
       articlePanierId: id.isNotEmpty ? id : (json['articlePanierId'] ?? ''),
       jouetId: json['jouetId'] ?? '',
@@ -56,14 +56,8 @@ class CartModel {
 
   int get nombreArticlesTotal =>
       articles.fold(0, (sum, item) => sum + item.quantite);
-      factory CartModel.fromFirestore(
-      String utilisateurId,
-      List<Map<String, dynamic>> documents,
-      List<String> ids) {
-    final items = <CartItemModel>[];
-    for (int i = 0; i < documents.length; i++) {
-      items.add(CartItemModel.fromFirestore(documents[i], ids[i], ids));
-    }
-    return CartModel(utilisateurId: utilisateurId, articles: items);
-  }
 }
+
+
+
+
